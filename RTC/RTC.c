ae2a230f47b8a6a4a350938ba52410d6f3ffebeb
SurionPL <surion.pl@gmail.com>
2015-09-22 02:16:15 +0200

    RTC Temp

--- RTC/RTC.c
+++ RTC/RTC.c
@@ -10,12 +10,31 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "I2C.h"
+#include "RTC.h"
 
 
 
 
-
-void RTC_Init()
+void RTC_Init(RTC_InitTypeDef* RTC_InitStruct)
 {
-	TWI_Init();
+
+}
+
+
+void RTC_GetTemp(int8_t* integer, uint8_t* fract)
+{
+
+	uint8_t temporary = 0;
+	TWI_Start();
+	TWI_Write_SLA(DS3231_SLA);
+	TWI_WriteByte(DS3231_TEMP_MSB);
+	TWI_RStart();
+	TWI_Write_SLA(DS3231_SLA + 1);
+	*integer = TWI_ReadByte_ACK();
+	temporary = TWI_ReadByte_NACK();
+	TWI_Stop();
+	if(temporary != 0)
+		*fract = (temporary >> 6) * 25;
+	else
+		*fract = 0;
 }
