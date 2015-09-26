75806361a2c5e96c676473f39581281fc37b1e7f
SurionPL <surion.pl@gmail.com>
2015-09-26 15:04:39 +0200

    RTC - set/get time

--- RTC/RTC.c
+++ RTC/RTC.c
@@ -13,7 +13,15 @@
 #include "RTC.h"
 
 void RTC_Init(RTC_InitTypeDef* RTC_InitStruct) {
+	uint8_t CR = 0;
 
+	if(RTC_InitStruct->RTC_Oscillator == DISABLE) {
+		CR |= EOSC;
+	}
+	else if(RTC_InitStruct->RTC_Oscillator == ENABLE)
+	{
+
+	}
 }
 
 void RTC_GetTemp(int8_t* integer, uint8_t* fract) {
@@ -39,4 +47,56 @@ uint8_t RTC_DecToBCD(uint8_t decimal) {
 
 uint8_t RTC_BCDToDec(uint8_t bcd) {
 	return ((((bcd >> 4) & 0x0F) * 10) | (bcd & 0x0F));
+}
+
+
+
+void RTC_SetTime(RTC_DateTimeTypedef* DateTimeStruct)
+{
+	uint8_t time[3];
+	time[0] = RTC_DecToBCD(DateTimeStruct->seconds);
+	time[1] = RTC_DecToBCD(DateTimeStruct->minutes);
+	time[2] = RTC_DecToBCD(DateTimeStruct->hours);
+	TWI_WriteBytes(DS3231_SLA, DS3231_SEC, 3, time);
+}
+
+void RTC_SetDate(RTC_DateTimeTypedef* DateTimeStruct)
+{
+	uint8_t date[4];
+	date[0] = RTC_DecToBCD(DateTimeStruct->day_of_week);
+	date[1] = RTC_DecToBCD(DateTimeStruct->day);
+	date[2] = RTC_DecToBCD(DateTimeStruct->month);
+	date[2] = RTC_DecToBCD(DateTimeStruct->year);
+	TWI_WriteBytes(DS3231_SLA, DS3231_DAY, 4, date);
+}
+
+void RTC_SetTimeDate(RTC_DateTimeTypedef* DateTimeStruct)
+{
+	RTC_SetTime(DateTimeStruct);
+	RTC_SetDate(DateTimeStruct);
+}
+
+void RTC_GetTime(RTC_DateTimeTypedef* DateTimeStruct)
+{
+	uint8_t time[3];
+	TWI_ReadBytes(DS3231_SLA, DS3231_SEC, 3, time);
+	DateTimeStruct->seconds = RTC_BCDToDec(time[0]);
+	DateTimeStruct->minutes = RTC_BCDToDec(time[1]);
+	DateTimeStruct->hours =   RTC_BCDToDec(time[2]);
+}
+
+void RTC_GetDate(RTC_DateTimeTypedef* DateTimeStruct)
+{
+	uint8_t date[4];
+	TWI_ReadBytes(DS3231_SLA, DS3231_SEC, 4, date);
+	DateTimeStruct->day_of_week = date[0];
+	DateTimeStruct->day   		= RTC_BCDToDec(date[1]);
+	DateTimeStruct->month 		= RTC_BCDToDec(date[2]);
+	DateTimeStruct->year  		= RTC_BCDToDec(date[3]);
+}
+
+void RTC_GetTimeDate(RTC_DateTimeTypedef* DateTimeStruct)
+{
+	RTC_GetTime(DateTimeStruct);
+	RTC_GetDate(DateTimeStruct);
 }
--- RTC/RTC.h
+++ RTC/RTC.h
@@ -14,7 +14,7 @@
 #define SQW_PIN  PB1
 #define SQW_PORT PORTB
 
-/* Timekeeping Registers */						/*  		Function			*/
+/* Timekeeping Registers *//*  		Function			*/
 #define DS3231_SEC			((uint8_t)0x00)		/*	Seconds						*/
 #define DS3231_MIN			((uint8_t)0x01)		/*	Minutes						*/
 #define DS3231_HRS			((uint8_t)0x02)		/*	Hours						*/
@@ -35,66 +35,88 @@
 #define DS3231_TEMP_MSB		((uint8_t)0x11)		/*	MSB of Temp					*/
 #define DS3231_TEMP_LSB		((uint8_t)0x12)		/*	LSB of Temp					*/
 
-#define EOSC 7//0x80
-#define BBSQW 6
-#define CONV 5
-#define RS2 4
-#define RS1 3
+
+//Alarm bits conf
+
+#define A1M1 ((uint8_t)0x80)
+#define A1M2 ((uint8_t)0x80)
+#define A1M3 ((uint8_t)0x80)
+#define A1M4 ((uint8_t)0x80)
+#define A2M2 ((uint8_t)0x80)
+#define A2M3 ((uint8_t)0x80)
+#define A2M4 ((uint8_t)0x80)
 
 
-typedef enum
-{
-	DISABLE = 0,
-	ENABLE = !DISABLE
+#define EOSC	((uint8_t)0x80)
+#define BBSQW	((uint8_t)0x40)
+#define CONV	((uint8_t)0x20)
+#define RS2		((uint8_t)0x10)
+#define RS1		((uint8_t)0x08)
+#define INTCN	((uint8_t)0x04)
+#define A2IE	((uint8_t)0x02)
+#define A1IE	((uint8_t)0x01)
 
-}NewState;
+typedef enum {
+	DISABLE = 0, ENABLE = !DISABLE
+
+} NewState;
 
 typedef struct {
 	uint8_t RTCCR;
 
 } RTC_TypeDef;
 
-
 typedef struct {
-	uint8_t seconds;
-	uint8_t minutes;
-	uint8_t hours;
-	uint8_t day;
-	uint8_t day_of_week;
-	uint8_t month;
+	uint8_t  seconds;
+	uint8_t  minutes;
+	uint8_t  hours;
+	uint8_t  day;
+	uint8_t  day_of_week;
+	uint8_t  month;
 	uint16_t year;
-	uint8_t century;
+	//uint8_t century;
 } RTC_DateTimeTypedef;
 
 //frequency of the square-wave outpu
-typedef enum
-{
+typedef enum {
 	Freq_1Hz = 0,
 	Freq_1024Hz,
 	Freq_4096Hz,
 	Freq_8192Hz
-}RTC_OutFreq_TypeDef;
+} RTC_OutFreqTypeDef;
 
+typedef enum {
+	Alarm1_OncePerSec = 0,
+	Alarm1_Sec,
+	Alarm1_MinSec,
+	Alarm1_HoursMinSec,
+	Alarm1_DateHoursMinSec,
+	Alarm1_DayHoursMinSec,
+	Alarm2_OncePerMin,
+	Alarm2_Min,
+	Alarm2_HoursMin,
+	Alarm2_DateHoursMin,
+	Alarm2_DayHoursMin,
+} RTC_AlarmTypeDef;
 
-
-typedef struct
-{
+typedef struct {
 	NewState RTC_Oscillator;
-	RTC_OutFreq_TypeDef RTC_OutFreq;
-	NewState RTC_Alarm1Int;
-	NewState RTC_Alarm2Int;
-
+	RTC_OutFreqTypeDef RTC_OutFreq;
+	//NewState RTC_Alarm1Int;
+	//NewState RTC_Alarm2Int;
+	NewState Interrupt;
+	RTC_AlarmTypeDef RTC_Alarm;
 
 } RTC_InitTypeDef;
 
-
-
-
-
 void RTC_Init(RTC_InitTypeDef* RTC_InitStruct);
-void RTC_SetDate(RTC_DateTimeTypedef*);
+void RTC_SetAlarm(RTC_InitTypeDef* RTC_InitStruct);
 void RTC_SetTime(RTC_DateTimeTypedef*);
-void RTC_SetDateTime(RTC_DateTimeTypedef*);
+void RTC_SetDate(RTC_DateTimeTypedef*);
+void RTC_SetTimeDate(RTC_DateTimeTypedef*);
+void RTC_GetTime(RTC_DateTimeTypedef*);
+void RTC_GetDate(RTC_DateTimeTypedef*);
+void RTC_GetTimeDate(RTC_DateTimeTypedef*);
 void RTC_GetTemp(int8_t* integer, uint8_t* fract);
 uint8_t RTC_DecToBCD(uint8_t decimal);
 uint8_t RTC_BCDToDec(uint8_t bcd);
