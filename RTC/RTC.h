81c0d5d29e772370e40cf49eddb1a9c142803bc0
SurionPL <surion.pl@gmail.com>
2015-09-30 00:17:54 +0200

    RTC - working but without comments

--- RTC/RTC.h
+++ RTC/RTC.h
@@ -9,10 +9,9 @@
 #define RTC_H_
 
 #define DS3231_SLA ((uint8_t)0xD0)				//Data write
-#define DS3231_R 0xD1
 
-#define SQW_PIN  PB1
-#define SQW_PORT PORTB
+//#define SQW_PIN  PB1
+//#define SQW_PORT PORTB
 
 /* Timekeeping Registers *//*  		Function			*/
 #define DS3231_SEC			((uint8_t)0x00)		/*	Seconds						*/
@@ -38,13 +37,14 @@
 
 //Alarm bits conf
 
-#define A1M1 ((uint8_t)0x80)
-#define A1M2 ((uint8_t)0x80)
-#define A1M3 ((uint8_t)0x80)
-#define A1M4 ((uint8_t)0x80)
-#define A2M2 ((uint8_t)0x80)
-#define A2M3 ((uint8_t)0x80)
-#define A2M4 ((uint8_t)0x80)
+#define A1M1  ((uint8_t)0x80)
+#define A1M2  ((uint8_t)0x80)
+#define A1M3  ((uint8_t)0x80)
+#define A1M4  ((uint8_t)0x80)
+#define A2M2  ((uint8_t)0x80)
+#define A2M3  ((uint8_t)0x80)
+#define A2M4  ((uint8_t)0x80)
+#define DY_DT ((uint8_t)0x40)
 
 
 #define EOSC	((uint8_t)0x80)
@@ -55,6 +55,31 @@
 #define INTCN	((uint8_t)0x04)
 #define A2IE	((uint8_t)0x02)
 #define A1IE	((uint8_t)0x01)
+
+
+//Days of week
+#define MONDAY	  ((uint8_t)0x01)
+#define TUESDAY   ((uint8_t)0x02)
+#define WEDNESDAY ((uint8_t)0x03)
+#define THURSDAY  ((uint8_t)0x04)
+#define FRIDAY 	  ((uint8_t)0x05)
+#define SATURDAY  ((uint8_t)0x06)
+#define SUNDAY    ((uint8_t)0x07)
+
+//Months
+#define JANUARY	  ((uint8_t)0x01)
+#define FEBRUARY  ((uint8_t)0x02)
+#define MARCH 	  ((uint8_t)0x03)
+#define APRIL  	  ((uint8_t)0x04)
+#define MAY 	  ((uint8_t)0x05)
+#define JUNE      ((uint8_t)0x06)
+#define JULY      ((uint8_t)0x07)
+#define AUGUST    ((uint8_t)0x08)
+#define SEPTEMBER ((uint8_t)0x09)
+#define OCTOBER   ((uint8_t)0x0A)
+#define NOVEMBER  ((uint8_t)0x0B)
+#define DECEMBER  ((uint8_t)0x0C)
+
 
 typedef enum {
 	DISABLE = 0, ENABLE = !DISABLE
@@ -73,50 +98,72 @@ typedef struct {
 	uint8_t  day;
 	uint8_t  day_of_week;
 	uint8_t  month;
-	uint16_t year;
-	//uint8_t century;
+	uint8_t year;
 } RTC_DateTimeTypedef;
 
-//frequency of the square-wave outpu
+//frequency of the square-wave output
 typedef enum {
-	Freq_1Hz = 0,
-	Freq_1024Hz,
-	Freq_4096Hz,
-	Freq_8192Hz
-} RTC_OutFreqTypeDef;
+	SQW_Disable = 0,
+	SQW_Freq_1Hz,
+	SQW_Freq_1024Hz,
+	SQW_Freq_4096Hz,
+	SQW_Freq_8192Hz
+} RTC_SQWFreqTypeDef;
 
 typedef enum {
-	Alarm1_OncePerSec = 0,
+	Alarm1_Disable = 0,
+	Alarm1_OncePerSec,
 	Alarm1_Sec,
 	Alarm1_MinSec,
 	Alarm1_HoursMinSec,
 	Alarm1_DateHoursMinSec,
 	Alarm1_DayHoursMinSec,
+} RTC_Alarm1TypeDef;
+
+
+typedef enum {
+	Alarm2_Disable = 0,
 	Alarm2_OncePerMin,
 	Alarm2_Min,
 	Alarm2_HoursMin,
 	Alarm2_DateHoursMin,
 	Alarm2_DayHoursMin,
-} RTC_AlarmTypeDef;
+} RTC_Alarm2TypeDef;
+
+
+typedef struct
+{
+	uint8_t CR;
+	RTC_Alarm1TypeDef RTC_Alarm1;
+	RTC_Alarm2TypeDef RTC_Alarm2;
+}RTC_RegistersTypeDef;
 
 typedef struct {
 	NewState RTC_Oscillator;
-	RTC_OutFreqTypeDef RTC_OutFreq;
-	//NewState RTC_Alarm1Int;
-	//NewState RTC_Alarm2Int;
-	NewState Interrupt;
-	RTC_AlarmTypeDef RTC_Alarm;
+	RTC_SQWFreqTypeDef RTC_SQW;
+	NewState BB_SQW;
+	RTC_Alarm1TypeDef RTC_Alarm1;
+	RTC_Alarm2TypeDef RTC_Alarm2;
 
 } RTC_InitTypeDef;
 
+typedef struct
+{
+	uint8_t  seconds;
+	uint8_t  minutes;
+	uint8_t  hours;
+	uint8_t  day;
+	uint8_t  day_of_week;
+}RTC_AlarmTypedef;
+
 void RTC_Init(RTC_InitTypeDef* RTC_InitStruct);
-void RTC_SetAlarm(RTC_InitTypeDef* RTC_InitStruct);
-void RTC_SetTime(RTC_DateTimeTypedef*);
-void RTC_SetDate(RTC_DateTimeTypedef*);
-void RTC_SetTimeDate(RTC_DateTimeTypedef*);
-void RTC_GetTime(RTC_DateTimeTypedef*);
-void RTC_GetDate(RTC_DateTimeTypedef*);
-void RTC_GetTimeDate(RTC_DateTimeTypedef*);
+void RTC_SetAlarm(RTC_AlarmTypedef* RTC_AlarmStruct);
+void RTC_SetTime(RTC_DateTimeTypedef* DateTimeStruct);
+void RTC_SetDate(RTC_DateTimeTypedef* DateTimeStruct);
+void RTC_SetTimeDate(RTC_DateTimeTypedef* DateTimeStruct);
+void RTC_GetTime(RTC_DateTimeTypedef* DateTimeStruct);
+void RTC_GetDate(RTC_DateTimeTypedef* DateTimeStruct);
+void RTC_GetTimeDate(RTC_DateTimeTypedef* DateTimeStruct);
 void RTC_GetTemp(int8_t* integer, uint8_t* fract);
 uint8_t RTC_DecToBCD(uint8_t decimal);
 uint8_t RTC_BCDToDec(uint8_t bcd);
